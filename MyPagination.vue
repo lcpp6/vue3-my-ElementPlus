0bfa8b7aaf3c5f2bdf7f68a7b8713735b82f2d9d
六只熊 <68930194+ZPanda-Liao@users.noreply.github.com>
2023-11-02 07:13:39 +0800

    Update MyPagination.vue

--- MyPagination.vue
+++ MyPagination.vue
@@ -4,7 +4,7 @@
   <MyPagination :currentPageNum="currentPageNum"
             :pageSize="pageSize"
             :total="total"
-            :smallSize="true"
+            :small="true"
             :pageSizeOptions="[10,20,30,40,50]"
             @update-currentPageNum="(e)=>currentPageNum=e"
             @update-pageSize="(e)=>pageSize=e"/>
@@ -14,7 +14,9 @@
 
     <!--每页 共多少条数据-->
     <div class="left-views" v-if="!showMoreLittle">
-      <span class="text-total-left">共 {{ total }} 条</span>
+      <span class="text-total">共</span>
+      <span style="color: #676767;margin: 0 3px 0 5px">{{ total }}</span>
+      <span class="text-total" style="margin-right: 10px"> 条</span>
       <!--输入设置页数-->
       <el-select v-model="pageSizeCopy" size="small" :disabled="disabledSelect"
                  :style="{width: getSelectAutoWidth+'px'}">
@@ -22,27 +24,27 @@
                    :label="item.label.length===1?('  '+item.label):(item.label)"
                    :value="item.value"/>
       </el-select>
-      <span class="text-total-right">条/页</span>
+      <span class="text-total-right" style="margin-left: 10px">条/页</span>
     </div>
     <!--pager-count官网最低只能传5 但此处我尝试传3，以便达到更好的缩小效果（有警告/但能用/可忽略）:current-page="currentPageNumCopy"-->
-    <el-pagination
-        v-model="currentPageNumCopy"
 
+    <el-pagination
+        :current-page="currentPageNumCopy"
         :page-size="pageSizeCopy"
         :total="total"
-        :pager-count="smallSize?3:5"
+        :pager-count="small?3:5"
         :hide-on-single-page="true"
-        :small="smallSize"
+        :small="small"
         layout="prev, pager, next"
         @size-change="(e)=>pageSizeCopy=e"
         @current-change="currentChange">
     </el-pagination>
+
     <div class="right-views">
       <!--选择前往第几页  (e) => e>0?currentPageNumCopy=(e>total?total:e):currentPageNumCopy=1 -->
-      <span class="text-page-left" v-if="!showMoreLittle">前往</span>
+      <span class="text-page" style="margin-right: 10px" v-if="!showMoreLittle">前往</span>
       <el-input-number v-model="inputNumber" :min="1" :max="maxPageNum" size="small" controls-position="right"
-                       :style="{width: getInputAutoWidth+'px'}" @input="handleInput"
-                       v-if="!showMoreLittle">
+                       @input="handleInput" v-if="!showMoreLittle">
       </el-input-number>
       <!--提示：由于v-model会更改当前页面，因此必须使用新变量，并添加一个事件来辅助修改（输入值为空的时候是null）
       而且不能使用change事件change需要按下回车键才触发(开发时候发现的，也许是一个bug)...-->
@@ -50,7 +52,7 @@
                        :style="{width: getInputAutoWidth+'px'}"
                        size="small" @input="handleInput" v-else>
       </el-input-number>
-      <span class="text-page-right" v-if="!showMoreLittle">页</span>
+      <span class="text-page" style="margin-left: 10px" v-if="!showMoreLittle">页</span>
     </div>
   </div>
 </template>
@@ -69,7 +71,7 @@ export default {
     total: {// 总共有多少条/行数据
       default: 0
     },
-    smallSize: {// 是否使用小尺寸的分页器(一般结合select使用)
+    small: {// 是否使用小尺寸的分页器
       default: false
     },
     pageSizeOptions: {// 可供选择的页面大小：5行一页、10行一页...
@@ -120,7 +122,7 @@ export default {
       return pageSizeOptions
     },
     showMoreLittle() {// 是否显示更小的分页器
-      return this.smallSize || Math.ceil(this.total / this.pageSizeCopy) <= 1
+      return this.small || Math.ceil(this.total / this.pageSizeCopy) <= 1
     },
     getSelectAutoWidth() {// 计算select的宽度
       return this.pageSizeOptionsFilter.length >= 1 ? (this.pageSizeOptionsFilter[this.pageSizeOptionsFilter.length - 1]).label.length * 8 + 35 : 8 + 35
@@ -150,21 +152,13 @@ export default {
     }
   },
   watch: {
-    currentPageNum: {// 随着父组件的currentPageNum变化而变化
-      handler(newVal) {
-        this.currentPageNumCopy = newVal
-      },
-      immediate: true,// 初始化后立即触发一次
+    currentPageNum(newVal) {
+      this.currentPageNumCopy = newVal
     },
-    pageSize: {// 随着父组件的pageSize变化而变化
-      handler(newVal) {
-        // console.log("pageSize = ", newVal)
-        this.pageSizeCopy = newVal
-      },
-      immediate: true,// 初始化后立即触发一次
+    pageSize(newVal) {
+      this.pageSizeCopy = newVal
     },
     currentPageNumCopy(newVal, oldVal) {
-      // console.log("当前页码 发生变化了...", newVal, oldVal)
       if (newVal !== oldVal) {// 告诉父组件 我改了currentPageNum----父组件需要使用它来更新 当前页的数据
         this.$emit('updateCurrentPageNum', newVal)
       }
@@ -184,36 +178,16 @@ export default {
   display: flex;
   flex-direction: row;
   justify-content: flex-end;
-  align-items: flex-end;
+  align-items: center;
   flex-wrap: nowrap;
   background-color: #ffffff;
   width: 100%;
   height: 30px;
 }
 
-@mixin text-mixin {
-  font-size: small;
-  display: flex;
-  flex-direction: row;
+span {
   user-select: none;
-}
-.text-total-left {
-  @include text-mixin;
-  margin: 0 10px 0 5px;
-}
-
-.text-total-right {
-  @include text-mixin;
-  margin: 0 5px 0 10px;
-}
-
-.text-page-left {
-  @include text-mixin;
-  margin: 0 10px 0 5px;
-}
-
-.text-page-right {
-  @include text-mixin;
-  margin: 0 5px 0 10px;
+  font-size: 13px;
+//text-align: center;
 }
 </style>
